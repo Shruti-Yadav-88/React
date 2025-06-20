1bf8cd41f595b5f78256c630205df75375233787
Shruti-Yadav-88 <shrutiyadav9849@gmail.com>
2025-06-20 08:30:24 +0545

    feat:update

--- A18ReactSetup/src/App.jsx
+++ A18ReactSetup/src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Son from './pages/son';
 import Daughter from './pages/daughter';
+import Son2 from './pages/Son2';
+import Daughter2 from './pages/Daughter2';
 
 
 function App() {
@@ -14,13 +16,18 @@ function App() {
   // const handleClick = () => {
   //   count<3 && setCount(count+1);
   // };
+
+  const[shoes,setShoes]=useState("sneakers")
+
   return(
-   <>
+   <div>
   
-     <Son meroBhag={jagga}/>
-     <Daughter meroProperty={jagga}/>
+     {/* <Son meroBhag={jagga}/>
+     <Daughter meroProperty={jagga}/> */}
+     <Son2 gift= {shoes}/>
+     <Daughter2 upahar = {shoes}/>
      
-    </>
+    </div>
    
    );
 };
---
+++ A18ReactSetup/src/pages/Daughter2.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Daughter2 = ({upahar}) => {
+  return (
+    <div>
+      I am Daughter component
+      <p>I got {upahar}</p>
+    </div>
+  )
+}
+
+export default Daughter2
